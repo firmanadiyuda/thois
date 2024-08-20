01abea124a15c954beb4d03cf25a220b4a52a84e
Firman Adiyuda <firmanadiyuda@gmail.com>
2024-08-20 17:05:04 +0000

    Modify InputComponent so it can be flexible with the field type

--- app/components/input_component.rb
+++ app/components/input_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class InputComponent < ViewComponent::Base
-  def initialize(form:, label:, data:, placeholder: "")
+  def initialize(form:, type:, label:, data:, placeholder: "")
     @form = form
+    @type = type
     @data = data
     @label = label
     @placeholder = placeholder
